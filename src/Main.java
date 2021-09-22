f236d8a15f006977066d2783692f5f3950ac2271
102s07 <102s07@H102R07.itaf.it.nrw.de>
2021-09-22 10:17:20 +0200

    Ausgabe Commit wurde hinzugefügt

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Die main Methode des Githubs Programms
+		System.out.printf("Hello GitHub%n");
+		System.out.printf("Ein Commit wurde erstellt%n");
+	}
+
+}
